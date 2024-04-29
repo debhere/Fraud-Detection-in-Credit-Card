b825775f7ed244386f5d2300041ee93d34bfc8c5
debhere <debmalya.mondal@outlook.com>
2024-04-29 20:19:02 +0530

    flask app integration

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import sys
 from src.logger import logging
 from src.exception import CustomException
 from src.utils.common import get_pickle
-import numpy
-from flask import Flask, render_template
+import numpy as np
+from flask import Flask, render_template, request
+from src.pipeline.prediction_pipeline import PlaygoundData, PredictPipeline
 
 app = Flask(__name__)
 
@@ -11,7 +12,58 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    try:
+        pg = PlaygoundData(
+            time = float(request.form.get('time')),
+            V1 = request.form.get('v1', type = float, default=np.nan),
+            V2 = request.form.get('v2', type = float, default=np.nan),
+            V3 = request.form.get('v3', type = float, default=np.nan),
+            V4 = request.form.get('v4', type = float, default=np.nan),
+            V5 = request.form.get('v5', type = float, default=np.nan),
+            V6 = request.form.get('v6', type = float, default=np.nan),
+            V7 = request.form.get('v7', type = float, default=np.nan),
+            V8 = request.form.get('v8', type = float, default=np.nan),
+            V9 = request.form.get('v9', type = float, default=np.nan),
+            V10 = request.form.get('v10', type = float, default=np.nan),
+            V11 = request.form.get('v11', type = float, default=np.nan),
+            V12 = request.form.get('v12', type = float, default=np.nan),
+            V13 = request.form.get('v13', type = float, default=np.nan),
+            V14 = request.form.get('v14', type = float, default=np.nan),
+            V15 = request.form.get('v15', type = float, default=np.nan),
+            V16 = request.form.get('v16', type = float, default=np.nan),
+            V17 = request.form.get('v17', type = float, default=np.nan),
+            V18 = request.form.get('v18', type = float, default=np.nan),
+            V19 = request.form.get('v19', type = float, default=np.nan),
+            V20 = request.form.get('v20', type = float, default=np.nan),
+            V21 = request.form.get('v21', type = float, default=np.nan),
+            V22 = request.form.get('v22', type = float, default=np.nan),
+            V23 = request.form.get('v23', type = float, default=np.nan),
+            V24 = request.form.get('v24', type = float, default=np.nan),
+            V25 = request.form.get('v25', type = float, default=np.nan),
+            V26 = request.form.get('v26', type = float, default=np.nan),
+            V27 = request.form.get('v27', type = float, default=np.nan),
+            V28 = request.form.get('v28', type = float, default=np.nan),
+            amount = request.form.get('amount', type = float, default=np.nan)
+        )
+        
+        df = pg.formulate()
+        
+        logging.info("Playground data is formatted...!!")
+        logging.info(df)
 
+        logging.info("Predict on the basis of user data...")
+        predict_pipeline = PredictPipeline()
+        prediction = predict_pipeline.get_prediction(df)
+
+        status = "valid" if prediction == 0 else "fraud"
+        #prediction_text = "The client is probably a"
+        return render_template('index.html', 
+                               prediction_text = f"The credit card transaction is {status}")
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,16 +1,31 @@
 import sys
+import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
+from src.utils.common import get_pickle
 
 
 class PredictPipeline:
-    pass
+    def __init__(self, model='HistGradientBoostingClassifier'):
+        self.dp = get_pickle()
+        self.model = get_pickle(model)
+    
+    def get_prediction(self, df):
+        try:
+            logging.info("Inside prediction pipeline")
+            data_trans = self.dp.transform(df)
+            pg_predict = self.model.predict(data_trans)
+            return pg_predict
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+
 
 
 class PlaygoundData:
     def __init__(
             self,
-            Time:int,
+            time:float,
             V1:float,
             V2:float,
             V3:float,
@@ -33,11 +48,88 @@ class PlaygoundData:
             V20: float,
             V21:float,
             V22:float,
-            V23: float,
+            V23:float,
             V24:float,
             V25:float,
             V26:float,
             V27:float,
             V28:float,
             amount:float
-            )
+            ):
+        self.time = time
+        self.V1 = V1
+        self.V2 = V2
+        self.V3 = V3
+        self.V4 = V4
+        self.V5 = V5
+        self.V6 = V6
+        self.V7 = V7
+        self.V8 = V8
+        self.V9 = V9
+        self.V10 = V10
+        self.V11 = V11
+        self.V12 = V12
+        self.V13 = V13
+        self.V14 = V14
+        self.V15 = V15
+        self.V16 = V16
+        self.V17 = V17
+        self.V18 = V18
+        self.V19 = V19
+        self.V20 = V20
+        self.V21 = V21
+        self.V22 = V22
+        self.V23 = V23
+        self.V24 = V24
+        self.V25 = V25
+        self.V26 = V26
+        self.V27 = V27
+        self.V28 = V28
+        self.amount = amount
+
+    def formulate(self):
+        try:
+            logging.info("Formulating the playground data in learnable format...!!!")
+            playgound_data_dict = {
+                "Time": [self.time],
+                "V1": [self.V1],
+                "V2": [self.V2],
+                "V3": [self.V3],
+                "V4": [self.V4],
+                "V5": [self.V5],
+                "V6": [self.V6],
+                "V7": [self.V7],
+                "V8": [self.V8],
+                "V9": [self.V9],
+                "V10": [self.V10],
+                "V11": [self.V11],
+                "V12": [self.V12],
+                "V13": [self.V13],
+                "V14": [self.V14],
+                "V15": [self.V15],
+                "V16": [self.V16],
+                "V17": [self.V17],
+                "V18": [self.V18],
+                "V19": [self.V19],
+                "V20": [self.V20],
+                "V21": [self.V21],
+                "V22": [self.V22],
+                "V23": [self.V23],
+                "V24": [self.V24],
+                "V25": [self.V25],
+                "V26": [self.V26],
+                "V27": [self.V27],
+                "V28": [self.V28],
+                "Amount": [self.amount]
+            }
+
+            columns = ["Time", "V1", "V2", "V3", "V4", "V5", 
+                       "V6", "V7", "V8", "V9", "V10", "V11", 
+                       "V12", "V13", "V14", "V15", "V16", "V17", 
+                       "V18", "V19", "V20", "V21", "V22", "V23", 
+                       "V24", "V25", "V26", "V27", "V28", "Amount"]
+            
+            return pd.DataFrame(playgound_data_dict, columns=columns)
+
+        except Exception as e:
+            raise CustomException(e, sys)
